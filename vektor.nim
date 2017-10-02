990c1d68525a3bcb14f44b03e5c030375eb132f9
Rudi Angela <olmeca@me.com>
2017-10-02 22:33:06 +0200

    Added check for input file existence. Added empty value processing (-e:0203, without =xxx). Added print command (not operational).

--- vektor.nim
+++ vektor.nim
@@ -8,9 +8,11 @@ type
       value: string
    
    MutSpecError = object of Exception
+   TCommand = enum
+      cmdModify, cmdPrint
 
 let helpText = """
-Usage: vektor -o:<output file> -e:<line element id>=<new value> [-e ...] <input file>
+Usage: vektor -o:<output file> -e:<line element id>[=<new value>] [-e ...] <input file>
 vektor: a tool for anonymizing Vektis EI standard compliant declaration files.
 It lets you specify an input file, a line element type and a value and then 
 creates a copy of the given file with all line elements of the given type 
@@ -23,15 +25,20 @@ var
    inputfile: string = nil
    outputPath: string = nil
    showHelp: bool = false
+   command: TCommand = cmdModify
 
 proc description(doctype: DocumentType): string =
-   "$#v$#.$#" % [doctype.name, intToStr(doctype.formatVersion), intToStr(doctype.formatSubVersion)]
+   "$# v$#.$#" % [doctype.name, intToStr(doctype.formatVersion), intToStr(doctype.formatSubVersion)]
+
+proc show(message: string) =
+   if command != cmdPrint:
+      echo message
+   else: discard
 
 proc fetch_doctype(line: string): DocumentType =
    let typeId = parseInt(line[2 .. 4])
    let version = parseInt(line[5 .. 6])
    let subversion = parseInt(line[7 .. 8])
-   echo "doctype: ", typeId, ", version: ", version, "sub: ", subversion
    get_doctype(typeId, version, subversion)
 
 proc getLineType(doctype: DocumentType, lineId: string, mutSpec: string): LineType =
@@ -51,32 +58,42 @@ proc getLineElementType(doctype: DocumentType, ltype: LineType, leId: string, mu
 proc createMutation(doctype: DocumentType, mutSpec: string): Mutation =
    let items: seq[string] = split(mutSpec, '=', 1)
    let leId = items[0]
-   let value = items[1]
+   var value: string = nil
    let lnId = leId[0..1]
    if leId.len != 4 or not leId.isDigit():
       raise newException(MutSpecError, "Line element ID in parameter: $# is not 4 digits." % [ mutSpec])
    else:
       let lineType = getLineType(doctype, lnId, mutSpec)
       let elemType = getLineElementType(doctype, lineType, leId, mutSpec)
-      if value.len > elemType.length:
-         raise newException(MutSpecError, "Specified value '$#' too large for element $#[$#] (max. length: $#)" % [value, leId, elemType.description, intToStr(elemType.length)])
-      elif elemType.fieldType == "N" and not isDigit(value):
-         raise newException(MutSpecError, "Invalid value '$#' for element $#[$#] (must be numerical)" % [value, leId, elemType.description, intToStr(elemType.length)])
-      else:
-         echo "Mutation for line $#, element $#, value: $#" % [lineType.name, elemType.description, value]
+      
+      # if a value was specified
+      if items.len > 1:
+         value = items[1]
+         if value.len > elemType.length:
+            raise newException(MutSpecError, "Specified value '$#' too large for element $#[$#] (max. length: $#)" % [value, leId, elemType.description, intToStr(elemType.length)])
+         elif elemType.fieldType == "N" and not isDigit(value):
+            raise newException(MutSpecError, "Invalid value '$#' for element $#[$#] (must be numerical)" % [value, leId, elemType.description, intToStr(elemType.length)])
+         show("Set element $# ($# - $#) to value: '$#'" % [leId, lineType.name, elemType.description, value])
          result = Mutation(lineId: lnId, leType: elemType, value: value)
+      # no value was specified, indicating the empty value
+      else: 
+         show("Set element $# ($# - $#) to empty value" % [leId, lineType.name, elemType.description])
+         result = Mutation(lineId: lnId, leType: elemType)
+      
 
 proc writeToFile(buf: seq[char], file: File) = 
    for c in buf:
       file.write(c)
-   file.write("\r\n")
+   file.write('\r')
+   file.write('\l')
 
 proc elementValue(fieldType: string, value: string, length: int): string = 
    if fieldType == "N":
-      let number = parseInt(value)
+      let number = if isNil(value): 0 else: parseInt(value)
       result = intToStr(number, length)
    else:
-      result = value & spaces(length - value.len)
+      let alphanum = if isNil(value): "" else: value
+      result = alphanum & spaces(length - alphanum.len)
 
 proc applyMutation(mut: Mutation, line: string, buf: var openArray[char]) =
    if mut.lineId == line[0..1]:
@@ -88,6 +105,14 @@ proc applyMutation(mut: Mutation, line: string, buf: var openArray[char]) =
       let newValueSeq = toSeq(newValue.items)
       for i in 0..(newValueSeq.len-1):
          buf[start+i] = newValueSeq[i]
+
+proc printLineElement(mut: Mutation, line: string) =
+   if mut.lineId == line[0..1]:
+      echo "Printing line."
+
+proc printLine(line: string) = 
+   for mutation in mutations:
+      printLineElement(mutation, line)
 
 proc mutateAndWrite(line: var string, file: File) =
    var buf = toSeq(line.mitems)
@@ -105,6 +130,8 @@ for kind, key, value in getopt():
          outputPath = value
       of "h", "help":
          showHelp = true
+      of "p", "print":
+         command = cmdPrint
    of cmdArgument:
       inputfile = key
    of cmdEnd: assert(false) # cannot happen
@@ -114,6 +141,8 @@ if showHelp:
 else:
    if isNil(inputfile) or inputfile.len == 0:
       quit("You need to specify an input file")
+   elif not existsFile(inputfile):
+      quit("Specified input file not found: '$#'" % [inputfile])
    elif mutationParams.len == 0:
       quit("You need to specify an element parameter (e.g. -e:0416=20190101).")
    else:
@@ -126,13 +155,22 @@ else:
             if line.startswith("01"):
                try:
                   let doctype = fetch_doctype(line)
+                  show("Document type: $#" % [description(doctype)])
                   mutations = lc[createMutation(doctype, mut) | (mut <- mutationParams), Mutation]
-                  var outputFile: File
-                  if open(outputFile, outputPath, fmWrite):
-                     mutateAndWrite(line, outputFile)
-                     while input.readLine(line):
+                  if command == cmdModify:
+                     var outputFile: File
+                     if open(outputFile, outputPath, fmWrite):
                         mutateAndWrite(line, outputFile)
-                     close(outputFile)
+                        while input.readLine(line):
+                           mutateAndWrite(line, outputFile)
+                        close(outputFile)
+                  elif command == cmdPrint:
+                     echo "Printing"
+                     printLine(line)
+                     while input.readLine(line):
+                        printLine(line)
+                  else:
+                     quit("Unknown command.")
                except Exception:
                   quit(getCurrentExceptionMsg())
             else:
