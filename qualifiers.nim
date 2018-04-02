6f511e023f632037d9fb1ccfb277de06046cb2a2
Rudi Angela <scm@olmeca.nl>
2018-04-02 22:57:54 +0200

    Refactored qualifier composition: And and Or only take two operands each. Operands can be And or Or composites. To specify a composite you must enclose in parentheses. Elementary (key-value) qualifiers don't need to be enclosed. Added support for spaces in qualifiers.

--- qualifiers.nim
+++ qualifiers.nim
@@ -1,59 +1,81 @@
-import pegs, strutils, future, logging
+import pegs, strutils, sequtils, lists, future, logging
 import "common", "doctypes", "context"
 
 type
    QualifierOperator = enum
       OpEquals, OpUnequal, OpGreaterThan, OpGreaterThanOrEqual, OpLessThan, OpLessThanOrEqual
+   CompositionOperator = enum
+      OpNone, OpAnd, OpOr
    
    LineQualifier* = ref LineQualifierObj
    LineQualifierObj* = object of RootObj
       qualifiesImpl: proc(q: LineQualifier, context: Context): bool 
+      toStringImpl: proc(q: LineQualifier): string
 
 proc qualifies*(q: LineQualifier, context: Context): bool =
    result = q.qualifiesImpl(q, context)
 
+proc `$`*(q:LineQualifier): string =
+   result = q.toStringImpl(q)
+
 type
-   KeyValueQualifier* = ref KeyValueQualifierObj
-   KeyValueQualifierObj* = object of LineQualifierObj
+   ElementaryQualifier* = ref ElementaryQualifierObj
+   ElementaryQualifierObj* = object of LineQualifierObj
       key*: string
       value*: string
       operator*: QualifierOperator
    
-   AndQualifier* = ref AndQualifierObj
-   AndQualifierObj* = object of LineQualifierObj
+   CompositeQualifier* = ref CompositeQualifierObj
+   CompositeQualifierObj* = object of LineQualifierObj
+      operator: CompositionOperator
       qualifiers: seq[LineQualifier]
-
-   OrQualifier* = ref OrQualifierObj
-   OrQualifierObj* = object of LineQualifierObj
-      qualifiers: seq[LineQualifier]
-
+      
 let cAndOperator = "&"
 let cOrOperator = "|"
 
-let keyValueQualifierPatternSpec = """
-Qualifier <- ^ {Key} {Operator} {Value} !.
+let elementaryQualifierPatternSpec = """
+Qualifier <- ^ Sp {Key} Sp {Operator} Sp {Value} Sp !.
 Key <- \d \d \d \d
 Value <- (!Operator .)+
 Operator <- '=' / '!=' / '<' / '>'
+Sp <- ' '*
 """
 
 let cCompositeQualifierPatternSpec = """
-Pattern <- ^ Composite !.
-Composite <- Parenthesized {Op} Parenthesized / ''
-Parenthesized <- Open {Sub} Close
-Sub <- (!Paren .)+ &Close / Composite
-Paren <- Open / Close
+Pattern <- ^ Sp Qualifier Sp !.
+Qualifier <- ElementaryQualifier / Open Composite Close 
+Composite <- Sp {Qualifier} Sp {Op} Sp {Qualifier} Sp / ''
+ElementaryQualifier <- Key Sp Comparator Sp Value
+Key <- \d \d \d \d
+Value <- Number / Text
+Comparator <- '=' / '!=' / '<' / '>'
+Number <- \d+
+Text <- '"' (!'"' .)* '"'
 Open <- '('
 Close <- ')'
 Op <- '&' / '|'
+Sp <- ' '*
 """
 
-let keyValueQualifierPattern = peg(keyValueQualifierPatternSpec)
+let elementaryQualifierPattern = peg(elementaryQualifierPatternSpec)
 let compositeQualifierPattern = peg(cCompositeQualifierPatternSpec)
 
+proc printElementaryQualifier(lq: LineQualifier): string =
+   if isNil(lq):
+      result = "<nil>"
+   else:
+      let kvq = ElementaryQualifier(lq)
+      result = "['$#' $# '$#']" % [kvq.key, $(kvq.operator), kvq.value]
+
+proc printCompositeQualifier(lq: LineQualifier): string =
+   if isNil(lq):
+      result = "<nil>"
+   else:
+      let cq = CompositeQualifier(lq)
+      result = "( $# $# $# )" % [$(cq.qualifiers[0]), $cq.operator, $(cq.qualifiers[1])]
 
 proc kvqQualify(lq: LineQualifier, context: Context): bool =
-   var kvq = KeyValueQualifier(lq)
+   var kvq = ElementaryQualifier(lq)
    let leValue = context.getElementValueString(kvq.key)
    case kvq.operator
    of OpEquals:
@@ -67,20 +89,18 @@ proc kvqQualify(lq: LineQualifier, context: Context): bool =
    else:
       raise newException(ValueError, "Operator not supported")
 
-proc andQualify(lq: LineQualifier, context: Context): bool =
-   var andQual = AndQualifier(lq)
-   for qualifier in andQual.qualifiers:
-      if not qualifier.qualifies(context):
-         return false
-   result = true
+proc andQualify(cq: CompositeQualifier, context: Context): bool =
+   all(cq.qualifiers, proc(lq: LineQualifier): bool = lq.qualifies(context))
 
-proc orQualify(lq: LineQualifier, context: Context): bool =
-   var orQual = OrQualifier(lq)
-   for qualifier in orQual.qualifiers:
-      if qualifier.qualifies(context):
-         return true
-   result = false
+proc orQualify(cq: CompositeQualifier, context: Context): bool =
+   any(cq.qualifiers, proc(lq: LineQualifier): bool = lq.qualifies(context))
 
+proc compositeQualify(lq: LineQualifier, context: Context): bool =
+   let cq = CompositeQualifier(lq)
+   if cq.operator == OpAnd:
+      result = andQualify(cq, context)
+   else:
+      result = orQualify(cq, context)
 
 proc operatorFromString(source: string): QualifierOperator =
    case source
@@ -95,46 +115,97 @@ proc operatorFromString(source: string): QualifierOperator =
    else:
       raise newException(ValueError, "Unknown operator: '$#'" % [source])
 
-proc newKeyValueQualifier*(key: string, op: QualifierOperator, value: string): KeyValueQualifier =
+proc compositionOperatorFromString(source: string): CompositionOperator =
+   case source
+   of "&":
+      result = OpAnd
+   of "|":
+      result = OpOr
+   else:
+      raise newException(ValueError, "Unknown composition operator: '$#'" % [source])
+
+proc newElementaryQualifier*(key: string, op: QualifierOperator, value: string): ElementaryQualifier =
    new(result)
    result.key = key
    result.operator = op
    result.value = value
    result.qualifiesImpl = kvqQualify
+   result.toStringImpl = printElementaryQualifier
+   debug("newElementaryQualifier -> $#" % [$result])
 
-proc newAndQualifier*(qualifiers: seq[LineQualifier]): AndQualifier =
+proc newCompositeQualifier*(): CompositeQualifier =
    new(result)
-   result.qualifiesImpl = andQualify
-   result.qualifiers = qualifiers
+   result.qualifiesImpl = compositeQualify
+   result.operator = OpNone
+   result.qualifiers = @[]
+   result.toStringImpl = printCompositeQualifier
 
-proc newOrQualifier*(qualifiers: seq[LineQualifier]): OrQualifier =
+proc newCompositeQualifier*(oper: CompositionOperator, qualifiers: seq[LineQualifier]): CompositeQualifier =
    new(result)
-   result.qualifiesImpl = orQualify
+   result.qualifiesImpl = compositeQualify
    result.qualifiers = qualifiers
+   result.operator = oper
+   result.toStringImpl = printCompositeQualifier
+   debug("newCompositeQualifier: $#" % [$result])
 
 proc newCompositeQualifier(operator: string, leftQual: LineQualifier, rightQual: LineQualifier): LineQualifier =
    let qualifiers = @[leftQual, rightQual]
-   result = LineQualifier(if operator == cAndOperator: newAndQualifier(qualifiers) else: newOrQualifier(qualifiers))
+   result = newCompositeQualifier(compositionOperatorFromString(operator), qualifiers)
+
+proc isFullyRead(cq: CompositeQualifier): bool =
+   cq.operator != OpNone and cq.qualifiers.len() == 2
+
+proc backtrack(cqStack: SinglyLinkedNode[CompositeQualifier]): SinglyLinkedNode[CompositeQualifier] =
+      result = cqStack
+      while result != nil and result.value.operator != OpNone and result.value.qualifiers.len() > 1:
+         debug("backtracking from $#" % $result.value)
+         result = result.next
+
+proc `&`(cqStack: SinglyLinkedNode[CompositeQualifier], qualSpec: string): SinglyLinkedNode[CompositeQualifier] =
+   debug ("Folding: '$#'" % qualSpec)
+   if qualSpec == cAndOperator or qualSpec == cOrOperator:
+      # If reading a composite operator and current cq already has an operator set then go up the tree
+      # you hit a node that is not completely read yet
+      result = cqStack
+      result.value.operator = compositionOperatorFromString(qualSpec)
+   elif qualSpec =~ elementaryQualifierPattern:
+      let kvq = newElementaryQualifier(matches[0], operatorFromString(matches[1]), matches[2])
+      assert (cqStack.value.qualifiers.len() < 2)
+      cqStack.value.qualifiers.add(kvq)
+      if cqStack.value.qualifiers.len() == 2:
+         debug("Done reading qualifier: $#" % [$cqStack.value])
+         result = backtrack(cqStack)
+      else:
+         result = cqStack
+   elif qualSpec =~ compositeQualifierPattern:
+      debug("Creating new composite for: $#" % qualSpec)
+      let newComp = newCompositeQualifier()
+      cqStack.value.qualifiers.add(newComp)
+      result = newSinglyLinkedNode(newComp)
+      result.next = cqStack
+   else:
+      raise newException(ValueError, "Unexpected value while parsing composite expression: '$#'" % qualSpec)
+
 
 proc parseQualifier*(docType: DocumentType, qualString: string): LineQualifier =
-   if qualString =~ compositeQualifierPattern:
+   debug("parseQualifier: '$#'" % qualString)
+   if qualString =~ elementaryQualifierPattern:
+      debug("parseQualifier: ElementaryQualifier '$#' '$#' '$#'" % matches)
+      result = newElementaryQualifier(matches[0], operatorFromString(matches[1]), matches[2])
+   elif qualString =~ compositeQualifierPattern:
       debug("parseQualifier found composite: [$#]" % [join(lc[m | (m <- matches, not isNil(m)), string], ", ")])
-      if matches.len > 2:
-         # For now we only support parsing a binary And/Or expression, with a left and a right operand.
-         # Both operands can themselves be And/Or expressions
-         #let qualifiers = lc[parseQualifier(docType, item) | (item <- matches, not isNil(item)), LineQualifier]
-         let leftQual = parseQualifier(docType, matches[0])
-         let rightQual = parseQualifier(docType, matches[2])
-         let operation = matches[1]
-         result = newCompositeQualifier(operation, leftQual, rightQual)
-   elif qualString =~ keyValueQualifierPattern:
-      debug("parseQualifier: '$#' '$#' '$#'" % matches)
-      let oper = operatorFromString(matches[1])
-      let key = matches[0]
-      let value = matches[2]
-      result = LineQualifier(newKeyVAlueQualifier(key, oper, value))
+      # cleanup matches to only contain kvq specs and composition operator specs
+      let prunedMatches = matches.filter(proc(m: string): bool = not isNil(m))
+      debug("parseQualifier prunedMatches: [$#]" % [join(lc[m | (m <- prunedMatches), string], ", ")])
+      let compQual = newCompositeQualifier()
+      let cqStack = newSinglyLinkedNode(compQual)
+      discard foldl(prunedMatches, a & b, cqStack)
+      debug("parseQualifier: composite: $#" % $compQual)
+      result = LineQualifier(compQual)
    else:
-      raise newException(ValueError, "Could not parse qualifier '$#'" % [qualString])
+      let error = "Could not parse qualifier '$#'" % [qualString]
+      debug("parseQualifier: $#" % error)
+      raise newException(ValueError, error)
 
 
 
